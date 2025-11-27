105b7208c360d5bd3d6975add978c8b3860658a0
MangozTechnology <mangoztechnology@gmail.com>
2025-11-26 20:47:34 -0600

    Create server.h

---
+++ server/server.h
@@ -0,0 +1,22 @@
+/*server.h*/
+
+#ifndef _SERVER_H_
+#define _SERVER_H_
+
+#include <string>
+
+class Server {
+public:
+   Server();
+   virtual ~Server();
+
+   virtual void serverGetType() = 0;
+   virtual void serverGetAPI() = 0;
+   virtual void serverConnectDatabase() = 0;
+   virtual void serverGetDatabase() = 0;
+   virtual void serverStreamMessage(const char* pMsg) = 0;
+   virtual void serverStreamMessageS(std::string& pStream) = 0;
+
+};
+
+#endif
